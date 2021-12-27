328f5e00dcdfc4618f6b5365740d5731a98ad925
Dmitry Korchagin <d.korchagin@corp.mail.ru>
2021-12-27 09:52:21 +0300

    add sleep in reader, check all items read from channel after close

--- go/channel_sync_echo/main.go
+++ go/channel_sync_echo/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 func reader(buffer <-chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for val := range buffer {
+		time.Sleep(1 * time.Second)
 		fmt.Printf("reader get: %d\n", val)
 	}
 	fmt.Println("reader done")
@@ -24,7 +26,7 @@ func writer(buffer chan<- int, count int, wg *sync.WaitGroup) {
 }
 
 func main() {
-	sChannel := make(chan int, 0)
+	sChannel := make(chan int, 5)
 	wg := &sync.WaitGroup{}
 
 	wg.Add(2)
