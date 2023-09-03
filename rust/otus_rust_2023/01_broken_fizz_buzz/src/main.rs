e2a1d712affe9201dfcf165c712ff30ba1e895b7
Dmitrii Korchagin <dmitrii.korchagin@booking.com>
2023-09-03 17:57:17 +0200

    fizbuz perf tests init

--- rust/otus_rust_2023/01_broken_fizz_buzz/src/main.rs
+++ rust/otus_rust_2023/01_broken_fizz_buzz/src/main.rs
@@ -1,17 +1,11 @@
+mod fizz_buzz;
 use std::vec::Vec;
 
 fn main() {
     const COUNT: usize = 100;
-    let mut result = Vec::with_capacity(COUNT);
-    for i in 1..=COUNT {
-        let s = match (i % 3, i % 5) {
-            (0, 0) => String::from("FizzBuzz"),
-            (0, _) => String::from("Fizz"),
-            (_, 0) => String::from("Buzz"),
-            (_, _) => format!("{}", i),
-        };
-        result.push(s);
-    }
+    let mut result = Vec::new();
+    result.resize(COUNT, String::from(""));
+    fizz_buzz::naive(&mut result);
     print_result(&result);
 }
 
