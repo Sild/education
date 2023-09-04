f149a672d21748172139babf3f745c5193c54563
Sild <sildtm@icloud.com>
2023-09-04 18:34:28 +0200

    some plays

--- rust/otus_rust_2023/01_broken_fizz_buzz/src/fizz_buzz.rs
+++ rust/otus_rust_2023/01_broken_fizz_buzz/src/fizz_buzz.rs
@@ -1,8 +1,4 @@
-#![feature(test)]
 use std::vec::Vec;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-
-extern crate test;
 
 pub fn naive(dst: &mut Vec<String>) {
     for i in 1..=dst.len() {
@@ -13,6 +9,60 @@ pub fn naive(dst: &mut Vec<String>) {
             (_, _) => format!("{}", i),
         };
         dst[i - 1] = s;
+    }
+}
+
+pub fn naive_unwrap(dst: &mut Vec<String>) {
+    let mut i = 1;
+
+    while i <= dst.len() {
+        let s = match (i % 3, i % 5) {
+            (0, 0) => String::from("FizzBuzz"),
+            (0, _) => String::from("Fizz"),
+            (_, 0) => String::from("Buzz"),
+            (_, _) => format!("{}", i),
+        };
+        i += 1;
+        if i == dst.len() {
+            break;
+        }
+        dst[i - 1] = s;
+        let s = match (i % 3, i % 5) {
+            (0, 0) => String::from("FizzBuzz"),
+            (0, _) => String::from("Fizz"),
+            (_, 0) => String::from("Buzz"),
+            (_, _) => format!("{}", i),
+        };
+        dst[i - 1] = s;
+        i += 1;
+    }
+}
+
+pub fn naive_print(max_num: usize) {
+    for i in 1..=max_num {
+        let _s = match (i % 3, i % 5) {
+            (0, 0) => String::from("FizzBuzz"),
+            (0, _) => String::from("Fizz"),
+            (_, 0) => String::from("Buzz"),
+            (_, _) => format!("{}", i),
+        };
+    }
+}
+
+pub fn naive_print2(max_num: usize) {
+    for i in 1..=max_num {
+        let mut s: String;
+        if i % 3 == 0 {
+            if i % 5 == 0 {
+                s = String::from("FizzBuzz")
+            } else {
+                s = String::from("Fizz")
+            }
+        } else if i % 5 == 0 {
+            s = String::from("Buzz")
+        } else {
+            s = format!("{}", i)
+        }
     }
 }
 
@@ -44,7 +94,22 @@ mod tests {
 
     #[bench]
     fn naive_perf(b: &mut Bencher) {
-        let mut dst = get_dst_store(100);
+        let mut dst = get_dst_store(1000);
         b.iter(|| naive(&mut dst));
+    }
+
+    #[bench]
+    fn naive_unwrap_perf(b: &mut Bencher) {
+        let mut dst = get_dst_store(1000);
+        b.iter(|| naive_unwrap(&mut dst));
+    }
+
+    #[bench]
+    fn naive_print_perf(b: &mut Bencher) {
+        b.iter(|| naive_print(1000));
+    }
+    #[bench]
+    fn naive_print2_perf(b: &mut Bencher) {
+        b.iter(|| naive_print2(1000));
     }
 }
--- rust/otus_rust_2023/01_broken_fizz_buzz/src/main.rs
+++ rust/otus_rust_2023/01_broken_fizz_buzz/src/main.rs
@@ -1,12 +1,18 @@
+#![feature(test)]
+extern crate test;
+
 mod fizz_buzz;
 use std::vec::Vec;
 
 fn main() {
-    const COUNT: usize = 100;
-    let mut result = Vec::new();
-    result.resize(COUNT, String::from(""));
-    fizz_buzz::naive(&mut result);
-    print_result(&result);
+    // const COUNT: usize = 100;
+    // let mut result = Vec::new();
+    // result.resize(COUNT, String::from(""));
+    // fizz_buzz::naive(&mut result);
+    // print_result(&result);
+
+    fizz_buzz::naive_print(1000);
+    fizz_buzz::naive_print2(1000);
 }
 
 fn print_result(result: &Vec<String>) {
