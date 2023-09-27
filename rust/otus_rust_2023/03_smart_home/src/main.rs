f32fb9a00d2bde1e5549f11b965eebaaca8b0c39
Sild <sildtm@icloud.com>
2023-09-27 17:49:23 +0200

    done smart_home

--- rust/otus_rust_2023/03_smart_home/src/main.rs
+++ rust/otus_rust_2023/03_smart_home/src/main.rs
@@ -1,36 +1,35 @@
-use std::collections::HashMap;
-use std::hash::Hash;
-use std::io::{Error, ErrorKind};
-use rand;
-use rand::Rng;
+use std::collections::{HashMap, HashSet};
 
+use std::io::{Error, ErrorKind};
+use std::ops::Index;
+use rand::Rng;
 
 const MAX_ROOMS: i32 = 4;
 const MAX_SOCKET_PER_ROOM: i32 = 4;
 const MAX_THERM_PER_ROOM: i32 = 4;
 
 struct SmartHouse {
-    rooms: HashMap<String, i32>, // fail to keep &str as key in sockets ¯\_(ツ)_/¯
-    sockets: HashMap<i32, Vec<SmartSocket>>,
-    thermometers: HashMap<i32, Vec<SmartThermometer>>,
+    rooms: HashSet<String>, // fail to keep &str as key in sockets ¯\_(ツ)_/¯
+    sockets: HashMap<String, Vec<SmartSocket>>,
+    thermometers: HashMap<String, Vec<SmartThermometer>>,
 }
 
-impl<'a> SmartHouse {
+impl SmartHouse {
     fn new() -> Self {
         let mut rng = rand::thread_rng();
         let rooms_cnt = rng.gen_range(1..=MAX_ROOMS);
-        let rooms = HashMap::from_iter((0..rooms_cnt).map(|i| (format!("Room_{}", i), i)));
+        let rooms = HashSet::from_iter((0..rooms_cnt).map(|i| (format!("Room_{}", i))));
         let mut sockets = HashMap::new();
         let mut thermometers = HashMap::new();
 
-        for (_, v) in rooms.iter() {
+        for v in rooms.iter() {
             let sock_cnt = rng.gen_range(1..=MAX_SOCKET_PER_ROOM);
             let room_sockets = Vec::from_iter(
                 (0..sock_cnt).map(|i| (
                     SmartSocket::new(format!("socket_{}", i))
                 ))
             );
-            sockets.insert(*v, room_sockets);
+            sockets.insert(v.clone(), room_sockets);
 
             let therm_cnt = rng.gen_range(1..=MAX_THERM_PER_ROOM);
             let therms = Vec::from_iter(
@@ -38,7 +37,7 @@ impl<'a> SmartHouse {
                     SmartThermometer::new(format!("termo_{}", i))
                 ))
             );
-            thermometers.insert(*v, therms);
+            thermometers.insert(v.clone(), therms);
         }
         SmartHouse {
             rooms,
@@ -47,28 +46,27 @@ impl<'a> SmartHouse {
         }
     }
 
-    fn get_rooms(&self) -> Vec<String> {
-        // It's awful. Must find a way to keep Vector instead of HashMap.
-        Vec::from_iter(self.rooms.keys().cloned())
+    fn get_rooms(&self) -> Vec<&str> {
+        Vec::from_iter(self.rooms.iter().map(|r| r.as_str()))
     }
 
-    fn devices(&self, room: &str) -> Result<Vec<&str>, Error> {
-        let index = match self.rooms.get(room.into()) {
-            Some(x) => x,
-            None => return Err(Error::new(ErrorKind::InvalidInput, "Room not found")),
+    fn devices(&self, room: &str) -> Result<Vec<String>, Error> {
+        if !self.rooms.contains(room) {
+            return Err(Error::new(ErrorKind::InvalidInput, "Room not found"));
         };
-        let mut res = Vec::from_iter(self.sockets[index].iter().map(|x| x.id.as_str()));
-        res.append(&mut Vec::from_iter(self.thermometers[index].iter().map(|x| x.id.as_str())));
+        let mut res = Vec::from_iter(self.sockets.index(room).iter().map(|x| format!("divice: {}, status: {:?}", x.id.as_str(), x.is_on)));
+        res.append(&mut Vec::from_iter(self.thermometers.index(room).iter().map(|x| format!("divice: {}, status: {:?}", x.id.as_str(), x.is_on))));
         Ok(res)
     }
 
-    fn create_report<T>(&self, provider: T) -> String
+    fn create_report<T>(&self, provider: &T) -> String
         where T: DeviceInfoProvider {
-        todo!("перебор комнат и устройств в них для составления отчёта")
+        provider.print_info()
     }
 }
 
 trait DeviceInfoProvider {
+    fn print_info(&self) -> String;
     // todo: метод, возвращающий состояние устройства по имени комнаты и имени устройства
 }
 
@@ -109,13 +107,22 @@ impl SmartThermometer {
 struct OwningDeviceInfoProvider {
     socket: SmartSocket,
 }
+
+impl DeviceInfoProvider for OwningDeviceInfoProvider {
+    fn print_info(&self) -> String {
+        format!("DeviceInfoProvider: socket_id: {}, socket_status: {:?}", self.socket.id, self.socket.is_on)
+    }
+}
 struct BorrowingDeviceInfoProvider<'a, 'b> {
     socket: &'a SmartSocket,
     thermo: &'b SmartThermometer,
 }
 
-
-// todo: реализация трейта `DeviceInfoProvider` для поставщиков информации
+impl DeviceInfoProvider for BorrowingDeviceInfoProvider<'_, '_> {
+    fn print_info(&self) -> String {
+        format!("BorrowingDeviceInfoProvider: socket_id: {}, socket_status: {:?}, therm_id: {}, term_status: {:?}", self.socket.id, self.socket.is_on, self.thermo.id, self.thermo.is_on)
+    }
+}
 
 fn main() {
     // Инициализация устройств
@@ -131,8 +138,7 @@ fn main() {
     let info_provider_1 = OwningDeviceInfoProvider {
         socket: socket1,
     };
-    // todo: после добавления обобщённого аргумента в метод, расскоментировать передачу параметра
-    // let report1 = house.create_report(/* &info_provider_1 */);
+    let report1 = house.create_report(&info_provider_1);
 
     // Строим отчёт с использованием `BorrowingDeviceInfoProvider`.
     let info_provider_2 = BorrowingDeviceInfoProvider {
@@ -140,9 +146,13 @@ fn main() {
         thermo: &thermo,
     };
     // todo: после добавления обобщённого аргумента в метод, расскоментировать передачу параметра
-    // let report2 = house.create_report(/* &info_provider_2 */);
+    let report2 = house.create_report(&info_provider_2);
 
     // Выводим отчёты на экран:
-    // println!("Report #1: {report1}");
-    // println!("Report #2: {report2}");
+    println!("Report #1: {report1}");
+    println!("Report #2: {report2}");
+
+    for room in house.get_rooms() {
+        println!("Devices in room {}: {}", room, house.devices(room).unwrap().join(", "));
+    }
 }
