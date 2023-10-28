9085ec036b8e26f9c6222acc3a3cf10f5fa793d5
Sild <sildtm@icloud.com>
2023-10-28 22:10:15 +0000

    add variadic constructor

--- cpp/plays/tmpl_fact_and_tuple.cpp
+++ cpp/plays/tmpl_fact_and_tuple.cpp
@@ -14,7 +14,7 @@ int fact<1>() {
 template<int Idx, typename T>
 auto& tuple_get(T& t) {
     if constexpr(Idx > 0) {
-        return tuple_get<Idx - 1, decltype(t.nest)>(t.nest);
+        return tuple_get<Idx - 1>(t.nest);
     }
     if constexpr(Idx == 0) {
         return t.cur;
@@ -23,12 +23,23 @@ auto& tuple_get(T& t) {
 
 template<typename T, typename... K>
 struct my_tuple {
+    my_tuple() = default;
+
+    my_tuple<T, K...>(const T& arg, K... other):
+      cur(arg)
+    , nest(other...)
+    {};
+
     T cur;
     my_tuple<K...> nest;
 };
 
 template<typename T>
 struct my_tuple<T> {
+    my_tuple() = default;
+
+    my_tuple<T>(const T& val): cur(val) {}
+
     T cur;
 };
 
@@ -43,6 +54,10 @@ int main() {
     std::cout << tuple_get<0>(t) << std::endl;
     std::cout << tuple_get<1>(t) << std::endl;
     std::cout << tuple_get<2>(t) << std::endl;
-    // std::cout << tuple_get<3, decltype(t)>(t) << std::endl;
+
+    auto t2 = my_tuple<int, float, std::string>(12, 22, "test2_string");
+    std::cout << tuple_get<0>(t2) << std::endl;
+    std::cout << tuple_get<1>(t2) << std::endl;
+    std::cout << tuple_get<2>(t2) << std::endl;
 }
 
