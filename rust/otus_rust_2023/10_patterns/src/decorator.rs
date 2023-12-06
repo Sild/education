3ce67dd05a63da924c1dd3c48aef54398ec9cfd8
Sild <sildtm@icloud.com>
2023-12-06 23:42:05 +0100

    impl dynamic decorator

--- rust/otus_rust_2023/10_patterns/src/decorator.rs
+++ rust/otus_rust_2023/10_patterns/src/decorator.rs
@@ -1,31 +1,54 @@
-pub trait DecTrait {
+pub trait MyTrait {
     fn foo(&self) -> i32;
 }
 
-pub struct LogDecorator<T> {
-    inner: T,
-}
-
-impl<T: DecTrait> LogDecorator<T> {
-    pub fn new(inner: T) -> Self {
-        Self { inner }
-    }
-}
-
-impl<T: DecTrait> DecTrait for LogDecorator<T> {
-    fn foo(&self) -> i32 {
-        println!("method foo called");
-        self.inner.foo()
-    }
-}
-
 #[derive(Default)]
-pub struct Pod {
+pub struct Pod {}
 
-}
-
-impl DecTrait for Pod {
+impl MyTrait for Pod {
     fn foo(&self) -> i32 {
         42
+    }
+}
+
+pub mod dec_static {
+    use crate::decorator::MyTrait;
+
+    pub struct LogDecorator<T> {
+        inner: T,
+    }
+
+    impl<T: MyTrait> LogDecorator<T> {
+        pub fn new(inner: T) -> Self {
+            Self { inner }
+        }
+    }
+
+    impl<T: MyTrait> MyTrait for LogDecorator<T> {
+        fn foo(&self) -> i32 {
+            println!("method foo called from static");
+            self.inner.foo()
+        }
+    }
+}
+
+pub mod dec_dyn {
+    use crate::decorator::MyTrait;
+
+    pub struct LogDecorator {
+        inner: Box<dyn MyTrait>,
+    }
+
+    impl LogDecorator {
+        pub fn new(inner: Box<dyn MyTrait>) -> Self {
+            Self { inner }
+        }
+    }
+
+    impl MyTrait for LogDecorator {
+        fn foo(&self) -> i32 {
+            println!("method foo called from dynamic");
+            self.inner.foo()
+        }
     }
 }
--- rust/otus_rust_2023/10_patterns/src/main.rs
+++ rust/otus_rust_2023/10_patterns/src/main.rs
@@ -1,18 +1,21 @@
 mod decorator;
 
-fn show_decorator() {
-    let execute = |obj: &dyn decorator::DecTrait| {
+fn run_decorators() {
+    let execute = |obj: &dyn decorator::MyTrait| {
         let res = obj.foo();
         println!("res={}", res);
     };
     let item = decorator::Pod::default();
     execute(&item);
 
-    let decorator = decorator::LogDecorator::new(item);
+    let decorator = decorator::dec_static::LogDecorator::new(item);
     execute(&decorator);
 
+    let item = decorator::Pod::default();
+    let decorator = decorator::dec_dyn::LogDecorator::new(Box::new(item));
+    execute(&decorator);
 }
 
 fn main() {
-    show_decorator();
+    run_decorators();
 }
