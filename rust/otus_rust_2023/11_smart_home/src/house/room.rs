269d9a70600a8ffaa094e6189f7fee733a623dbe
Sild <sildtm@icloud.com>
2023-12-20 18:45:32 +0100

    use thiserror trait

---
+++ rust/otus_rust_2023/11_smart_home/src/house/room.rs
@@ -0,0 +1,59 @@
+use crate::house::errors::HouseError;
+use crate::house::traits::{DeviceVisitor, SmartDevice};
+use std::any::Any;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct Room {
+    name: String,
+    pub(crate) devices: HashMap<String, Box<dyn Any + Send + Sync>>, // device_id->device
+}
+
+impl Room {
+    pub fn new(name: &str) -> Self {
+        Room {
+            name: name.to_string(),
+            devices: HashMap::new(),
+        }
+    }
+    pub(crate) fn add_device<T: SmartDevice + 'static>(
+        &mut self,
+        device: T,
+    ) -> Result<(), HouseError> {
+        if self.devices.contains_key(device.get_id()) {
+            return Err(HouseError::DeviceAlreadyExists(device.get_id().to_string()));
+        }
+        self.devices
+            .insert(device.get_id().to_string(), Box::new(device));
+        Ok(())
+    }
+
+    pub(crate) fn extract_device<T: 'static>(&mut self, device_id: &str) -> Result<T, HouseError> {
+        match self.devices.remove(device_id) {
+            Some(any_device) => {
+                match any_device.downcast::<T>() {
+                    Ok(device) => Ok(*device),
+                    Err(any_device_back) => {
+                        // not grate - push device back to storage ¯\_(ツ)_/¯
+                        self.devices.insert(device_id.to_string(), any_device_back);
+                        Err(HouseError::WrongDeviceType(device_id.to_string()))
+                    }
+                }
+            }
+            None => Err(HouseError::DeviceNotFound(device_id.to_string())),
+        }
+    }
+
+    pub(crate) fn visit_devices_mut<T: DeviceVisitor>(&mut self, visitor: &mut T) {
+        for d in self.devices.values_mut() {
+            // it's tricky, visitor.visit() can accept anything. We must unwrap the Box before call.
+            visitor.visit_mut(self.name.as_str(), &mut (**d))
+        }
+    }
+
+    pub fn visit_devices<T: DeviceVisitor>(&self, visitor: &mut T) {
+        for d in self.devices.values() {
+            visitor.visit(self.name.as_str(), &(**d))
+        }
+    }
+}
