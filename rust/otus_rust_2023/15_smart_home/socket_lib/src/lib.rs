c3bd86f69130da12c3fc0788dcc716d22e5f3523
Sild <sildtm@icloud.com>
2024-02-12 22:29:37 +0100

    init lib

---
+++ rust/otus_rust_2023/15_smart_home/socket_lib/src/lib.rs
@@ -0,0 +1,49 @@
+use rand::Rng;
+
+pub struct Socket {
+    id: String,
+    pub is_on: bool,
+}
+
+impl Socket {
+    pub fn new(id: &str) -> Self {
+        Self { id: id.to_string(), is_on: false }
+    }
+
+    pub fn get_report(&self) -> String {
+        let mut rng = rand::thread_rng();
+        let power = match self.is_on {
+            true => rng.gen::<f64>(),
+            false => 0.0,
+        };
+        format!(
+            "type: socket_lib, id: {}, is_on: {}, power: {power:.2}",
+            self.id, self.is_on
+        )
+    }
+}
+
+static mut SOCKET: Option<Socket> = None;
+
+unsafe fn init_socket() {
+    if SOCKET.is_none() {
+        SOCKET = Some(Socket::new("lib-socket_lib"));
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn switch_socket_status() {
+
+    unsafe {
+        init_socket();
+        SOCKET.as_mut().unwrap().is_on = !SOCKET.as_ref().unwrap().is_on;
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn print_socket_report() {
+    unsafe {
+        init_socket();
+        println!("{}", SOCKET.as_ref().unwrap().get_report());
+    }
+}
---
+++ rust/otus_rust_2023/15_smart_home/src/main.rs
@@ -0,0 +1,46 @@
+use std::io::{stdin, stdout, Write};
+
+extern "C" {
+    pub fn switch_socket_status();
+    pub fn print_socket_report();
+}
+
+fn switch_socket_status_safe() {
+    unsafe {
+        switch_socket_status();
+    }
+}
+
+fn print_socket_report_safe() {
+    unsafe {
+        print_socket_report();
+    }
+}
+
+fn main() {
+    println!("Socket cli started!");
+    let mut user_input=String::new();
+
+    loop {
+        print!("s - switch status, r - get report, q - quit. Your input: ");
+        let _ = stdout().flush();
+        user_input.clear();
+        stdin().read_line(&mut user_input).expect("Did not enter a correct string");
+        match user_input {
+            ref ui if ui.trim() == "s" => {
+                switch_socket_status_safe();
+                println!("Success!");
+            }
+            ref ui if ui.trim() == "r" => {
+                print_socket_report_safe();
+            }
+            ref ui if ui.trim() == "q" => {
+                println!("Goodbye!");
+                break;
+            }
+            _ => {
+                println!("Unknown input!");
+            }
+        }
+    }
+}
